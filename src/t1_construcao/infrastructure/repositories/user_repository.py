d158d47d5fd7bb27f80881160d86af6239f3469d
Pedro <pedroh.fsouzars@gmail.com>
2025-08-19 16:43:24 -0300

    feat: basic repository methods

--- src/t1_construcao/infrastructure/repositories/user_repository.py
+++ src/t1_construcao/infrastructure/repositories/user_repository.py
@@ -1,5 +1,14 @@
 from ._repository_meta import RepositoryMeta
-
+from t1_construcao.domain import UserEntity
+from ..models import User
+from .mappers import user_model_to_entity
 
 class UserRepository(metaclass=RepositoryMeta):
-    pass
+    
+    async def create(self, name: str) -> UserEntity:
+        user = await User.create(name=name)
+        return user_model_to_entity(user)
+
+    async def get_by_id(self, user_id: str) -> UserEntity | None:
+        user = await User.get(id=user_id)
+        return user_model_to_entity(user) if user else None
